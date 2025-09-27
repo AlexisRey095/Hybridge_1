1f9d16d6daa047a70beb08610727e9529d3a7ca2
AlexisRey095 <reyesanchez.alexis@gmail.com>
2025-09-26 21:57:25 -0600

    ejercicios listas1

---
+++ Universidad/1er_Cuatri/Python/Libro Python/listas.py
@@ -0,0 +1,11 @@
+#ejercicio para trabajar lo trabajo en la seccion 3 del libro
+
+places = ['tlaxcala', 'veracruz', 'guadalajara', 'tequila', 'durango']
+print (places)
+print (sorted(places, reverse=True))
+print (places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+print(len(places))
