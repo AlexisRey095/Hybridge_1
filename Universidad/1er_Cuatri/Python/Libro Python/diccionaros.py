45438441d53ba647e0bf24906bf9d50ff6715ea7
Alexis <reyesanchez.alexis@gmail.com>
2025-10-05 16:13:13 -0600

    avance de libro 05_10_PC

---
+++ Universidad/1er_Cuatri/Python/Libro Python/diccionaros.py
@@ -0,0 +1,15 @@
+ #Diccionaro de Karen
+
+persona_fav = {
+    'first_name':'Karen',
+    'second_name':'Cabrera',
+    'city':'Martinez de la torre',
+    'age':25,
+    'childrens':'yes'
+    }
+
+print(persona_fav['first_name'])
+print(persona_fav['second_name'])
+print(persona_fav['city'])
+print(persona_fav['age'])
+print(persona_fav['childrens'])
