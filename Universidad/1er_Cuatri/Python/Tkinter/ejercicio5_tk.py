24c34859ea2c16d7e0495fb990570d262041a1ca
AlexisRey095 <reyesanchez.alexis@gmail.com>
2025-10-21 20:03:11 -0600

    reloj arena 21_10

---
+++ Universidad/1er_Cuatri/Python/Tkinter/ejercicio5_tk.py
@@ -0,0 +1,18 @@
+
+from tkinter import *
+import time
+
+def actualizar_hora():
+    hora = time.strftime("%H:%M:%S")
+    variable_control.set(hora)
+    root.after(1000, actualizar_hora)
+
+root = Tk()
+root.resizable(False, False)
+variable_control = StringVar()
+
+reloj = Label(textvariable= variable_control, fg="red",
+              font=("Arial", 18), padx=20, pady=20)
+
+reloj.pack()
+actualizar_hora()
